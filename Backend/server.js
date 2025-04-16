92369292ece7e6887dac3a52c21f35d98290dbba
Shikhar <ashikhar762@gmail.com>
2025-04-16 10:47:11 +0530

    Commit

--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,10 @@ import app from "./app.js";
 
 const port  = process.env.PORT;
 
-
+app.get("/", (req, res) => {
+    res.send("Backend is live 🚀");
+  });
+  
 //start server at port 
 app.listen(port,()=>{
     console.log(`server is running on port ${port}`);
